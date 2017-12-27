b53f0667af5be355a516d9d4bcf65882eba85117
uzimith <uzimith.x9@gmail.com>
2017-12-28 08:59:00 +0900

    init commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+)
+
+var (
+	source      = flag.String("source", "", "(source mode) Input Go source file; enables source mode.")
+	destination = flag.String("destination", "", "Output file; defaults to stdout.")
+)
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *source != "" {
+	} else {
+		if flag.NArg() != 2 {
+			usage()
+			log.Fatal("Expected exactly two arguments")
+		}
+	}
+}
+
+func usage() {
+	io.WriteString(os.Stderr, usageText)
+	flag.PrintDefaults()
+}
+
+const usageText = `struct-athena
+Example:
+	struct-athena -source=foo.go [other options]
+`
